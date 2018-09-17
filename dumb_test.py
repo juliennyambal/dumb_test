fecda303f95a69997e94e3e1d80bcc4406388c0c
juliennyambal <juliennyambal@gmail.com>
2018-09-17 10:54:54 +0200

    message

---
+++ dumb_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 10:40:23 2018
+
+@author: julien
+"""
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(4) == 5
